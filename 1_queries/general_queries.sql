c1a0c9e01f8db9b8b9799869ba42393238827263
CDNDragoon <tjgossage@gmail.com>
2021-05-26 18:10:17 +0000

    queries 2

--- 1_queries/general_queries.sql
+++ 1_queries/general_queries.sql
@@ -9,3 +9,7 @@ select name, id, email, cohort_id from students where email not like '%gmail%' a
 select name id, cohort_id from students where end_date is null order by cohort_id;
 
 select name, email, phone from students where end_date is not null and github is null;
+
+SELECT sum(assignment_submissions.duration) as total_duration FROM assignment_submissions JOIN students ON students.id = student_id WHERE students.name = 'Ibrahim Schimmel';
+
+SELECT sum(assignment_submissions.duration) as total_duration FROM assignment_submissions JOIN students ON students.id = student_id JOIN cohorts ON cohorts.id = cohort_id WHERE cohorts.name = 'FEB12';
